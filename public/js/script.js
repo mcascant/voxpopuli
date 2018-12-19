073f1bbc7c164855908517f683982a3ed3b1b777
jonatuxdr <jonathan.deriemaecker@gmail.com>
2018-12-19 17:48:59 +0100

    changes voters

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,10 @@
 /**
  * Created by developer on 12/13/18.
  */
-function voteCall(postId) {
-    console.log('STEP')
-    let xhttp = new XMLHttpRequest();
-    let url = '/vote/create/' + postId;
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-            rsp = JSON.parse(this.responseText)
-        }
-    };
-    xhttp.open("GET", url, true);
-    xhttp.send();
-}
+let clicks = 0;
+function click(clicks){
+        clicks += 1;
+        console.log("YEAH");
+        let element = document.querySelector("#" + name);
+        element.querySelector("#valueCount").innerHTML = clicks;
+};
